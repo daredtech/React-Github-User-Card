267ed2c3e734246d1651fd48f3d67a153f5104f3
Red <vhanchar@ucsc.edu>
2019-08-06 12:49:58 -0700

    created initial UserCard component and rendered it

---
+++ github-user-card/src/Components/UserCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class UserCard extends React.Component{
+
+    render (){
+        return (
+            <div className='user-card'>
+                I am user card
+            </div>
+        )
+    }
+
+} 
+
+export default UserCard;
